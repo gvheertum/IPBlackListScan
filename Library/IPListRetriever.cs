ede9c25f111f9fa5d133caa2a671b176fe967564
Gertjan <git@netietsmeer.nl>
2019-07-28 20:29:13 +0200

    System now reads the ip.list file

--- Library/IPListRetriever.cs
+++ Library/IPListRetriever.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 namespace IPBlackListCheck.Library
 {
     public class IPListRetriever
@@ -11,17 +15,48 @@ namespace IPBlackListCheck.Library
 			return c;
 		}
 
-		public IEnumerable<IPCheck> GetItemsForTest()
+		private IEnumerable<IPCheck> GetItemsForTest()
 		{
 			yield return new IPCheck("Barracuda test (should alwas show as blacklisted)", "127.0.0.2");
 		}
 
-		public IEnumerable<IPCheck> GetItemsFromIpListFile() 
+        private const string IpFileListName = "ip.list";
+
+		private IEnumerable<IPCheck> GetItemsFromIpListFile() 
 		{
 			//Read the file ip.list
 			//Format: IP;Name
-			return new List<IPCheck>();
-			//return new IPCheckList().GetCheckItems();
+            string scanPath = Path.Combine(GetExecutionPath(), IpFileListName);
+
+            if(!System.IO.File.Exists(scanPath))
+            {
+                System.Console.WriteLine($"No ip.list found, expected: {scanPath}");
+                yield break;
+            }
+
+            System.Console.WriteLine($"Using IP list: {scanPath}");
+            var lines = System.IO.File.ReadAllLines(scanPath).Where(l => !string.IsNullOrWhiteSpace(l));
+            System.Console.WriteLine($"Found {lines.Count()} lines in ip.list");
+            foreach(var l in lines)
+            {
+                yield return ReadFromLine(l);
+            }
 		}
+        private IPCheck ReadFromLine(string line)
+        {
+            //Expecting calling code to do sanity checks
+            if(string.IsNullOrWhiteSpace(line)) { throw new Exception("Empty line, sorry"); }
+            var tokens = line.Split(new string[] {";"}, 2, StringSplitOptions.RemoveEmptyEntries);
+            return new IPCheck()
+            {
+                IPToCheck = tokens[0],
+                Name = tokens.Length > 1 ? tokens[1] : $"{tokens[1]} (no name specified)"
+            };
+        }
+
+        private string GetExecutionPath()
+        {
+            return new System.IO.FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).DirectoryName;
+        }
 	}
 }
